37223b98494b870b50fdc9b6af3ac497c6f66388
arsen-tedeev <arsentedeev455@gmail.com>
2023-07-05 00:18:40 +0300

    Deleted the file from the git repository

--- scripts/py-scr/class_1_selection.py
+++
@@ -1,9 +0,0 @@
-from Bio import SeqIO
-hla_class_1 = open("./output/hla_class_1.fasta", 'w+')
-
-record_dict = SeqIO.to_dict(SeqIO.parse("./data/hla_prot.fasta", "fasta"))
-for i in record_dict.keys():
-    if " A*" in record_dict[i].description or " B*" in record_dict[i].description or " C*" in record_dict[i].description:
-        hla_class_1.write(record_dict[i].description + "\n")
-
-hla_class_1.close()
